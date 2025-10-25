c460c79882ec698f3602e95f583af7bfec364c57
MIXVBRC <mixvbrc@gmail.com>
2025-10-26 09:43:54 +1000

    fix

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php require __DIR__ . '/vendor/autoload.php';
 
-$git = json_decode(file_get_contents(__DIR__ . '/git.json'));
+$git = json_decode(file_get_contents(__DIR__ . '/git.json'), true);
 
 if (!empty($git)) {
 
